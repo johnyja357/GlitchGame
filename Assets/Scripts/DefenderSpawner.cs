28ada2ed118544afd8c7131ff82fff8f0bf8929a
DESKTOP-S9A09IT\Giannis <giannis@machapp.net>
2018-11-15 13:22:37 +0200

    initial commit, added complete project to git

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour {
+
+    public Camera myCamera;
+
+    private GameObject parent;
+    private StarDisplay starDisplay;
+
+    void Start()
+    {
+        parent = GameObject.Find("Defenders");
+        starDisplay = GameObject.FindObjectOfType<StarDisplay>();
+
+        if (!parent)
+        {
+            parent = new GameObject("Defenders");
+        }
+    }
+
+    void OnMouseDown()
+    {
+        Vector2 rawPos = CalculateWorldPointOfMouseClick();
+        Vector2 roundedPos = SnapToGrid(rawPos);
+        GameObject defender = Button.selectedDefender;
+
+        int defenderCost = defender.GetComponent<Defender>().starCost;
+
+        if (starDisplay.UseStars(defenderCost) == StarDisplay.Status.SUCCESS)
+        {
+            SpawnDefender(roundedPos, defender);
+        }
+        else
+        {
+            Debug.Log("Insufficient stars to spawn");
+        }
+
+    }
+
+    private void SpawnDefender(Vector2 roundedPos, GameObject defender)
+    {
+        Quaternion zeroRotation = Quaternion.identity;
+        GameObject newDefender = Instantiate(defender, roundedPos, zeroRotation) as GameObject;
+        newDefender.transform.parent = parent.transform;
+    }
+
+    Vector2 SnapToGrid (Vector2 rawWaroldPos)
+    {
+        float newX = Mathf.RoundToInt(rawWaroldPos.x);
+        float newY = Mathf.RoundToInt(rawWaroldPos.y);
+
+        return new Vector2(newX, newY);
+    }
+
+    Vector2 CalculateWorldPointOfMouseClick()
+    {
+        float mouseX = Input.mousePosition.x;
+        float mouseY = Input.mousePosition.y;
+        float distanceFromCamera = 10f;
+
+        Vector3 weirdTriplet = new Vector3(mouseX, mouseY, distanceFromCamera);
+        Vector2 worldPos = myCamera.ScreenToWorldPoint(weirdTriplet);
+
+        return worldPos;
+    }
+}
---
+++ Assets/Scripts/Stone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : MonoBehaviour {
+
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        Attacker attacker = collision.gameObject.GetComponent<Attacker>();
+
+        if (attacker)
+        {
+            animator.SetTrigger("underAttack Trigger");
+        }
+    }
+
+}
